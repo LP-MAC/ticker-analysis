e4108772e205ce2ef581eef1935989fd95ff4b94
Luigi Pantisano <Luigi@Luigis-MacBook-Pro-M4.local>
2026-05-19 08:04:22 -0700

    Use yahoo-finance2 as static object

--- server.js
+++ server.js
@@ -30,17 +30,8 @@ try {
   ];
 }
 
-const yahooFinance = new yahooFinanceLib({
-  fetchOptions: {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Cookie': 'B=consent; GUC=consent'
-    }
-  }
-});
+// Use yahoo-finance2 as static object (v3 supports both patterns)
+const yahooFinance = yahooFinanceLib;
 
 const app = express();
 app.use(cors());
